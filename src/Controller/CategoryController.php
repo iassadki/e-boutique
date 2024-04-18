02313c47e79fae7b9ceaf55192d8eed5dfd5b60d
iassadki <ilias.assadki@gmail.com>
2024-04-18 23:19:51 +0200

    push

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -43,10 +44,11 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
+    public function show(Category $category, MediaRepository $mediaRepository): Response
     {
         return $this->render('category/show.html.twig', [
             'category' => $category,
+            'img' => $mediaRepository->findAll()
         ]);
     }
 
