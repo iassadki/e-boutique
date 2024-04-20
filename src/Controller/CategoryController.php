1c8710f8762e0838059b84439be7fd18e222687b
iassadki <ilias.assadki@gmail.com>
2024-04-20 17:21:23 +0200

    debug

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -46,12 +46,9 @@ class CategoryController extends AbstractController
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
     public function show(Category $category, MediaRepository $mediaRepository): Response
     {
-        // Récupérer les trois premières images pour la catégorie spécifiée
-        $images = $mediaRepository->findThreeImagesForCategory($category->getId());
-
         return $this->render('category/show.html.twig', [
             'category' => $category,
-            'images' => $images,
+            'img' => $mediaRepository->findAll()
         ]);
     }
 
